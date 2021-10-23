283da71a2c3d04ec708a5ecf761e386e8800d860
pikokr <parangee9706@gmail.com>
2021-10-23 11:12:53 +0900

    page loading

---
+++ components/PageEditor.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { Editor as GrapesJsEditor } from 'grapesjs'
+import grapesjs from 'grapesjs'
+import axios from 'axios'
+import 'grapesjs-preset-webpage'
+
+const PageEditor: React.FC<{ id: string; data: any }> = ({ id, data }) => {
+  const [editor, setEditor] = React.useState<GrapesJsEditor | null>(null)
+
+  React.useEffect(() => {
+    if (!editor) {
+      const editor = grapesjs.init({
+        container: `#page-editor-${id}`,
+        storageManager: {
+          type: 'vega',
+          autoload: true
+        },
+        height: '100%',
+        plugins: ['grapesjs-preset-webpage', 'gjs-blocks-basic']
+      })
+
+      editor.StorageManager.add('vega', {
+        async load(keys: string[], clb: any, clbErr: any) {
+          console.log(keys)
+          try {
+            const { data } = await axios.get<Record<string, any>>(
+              `/api/admin/pages/${id}`
+            )
+            const res: Record<string, any> = {}
+            keys.forEach((key) => {
+              const value = data[key]
+              if (value) {
+                res[key] = value
+              }
+            })
+
+            clb(res)
+          } catch (e) {
+            clbErr(e)
+          }
+        },
+        async store(data: any, clb: any, clbErr: any) {
+          try {
+            await axios.post(`/api/admin/pages/${id}/store`, {
+              components: JSON.parse(data['gjs-components']),
+              styles: JSON.parse(data['gjs-styles'])
+            })
+            clb()
+          } catch (e) {
+            clbErr(e)
+          }
+        }
+      })
+
+      editor.setComponents(data.components)
+      editor.setStyle(data.styles)
+    }
+  }, [])
+
+  return <div id={`page-editor-${id}`} />
+}
+
+export default PageEditor
--- pages/admin/pages/[id]/editor.tsx
+++ pages/admin/pages/[id]/editor.tsx
@@ -2,19 +2,13 @@ import React from 'react'
 import { requireAdmin } from '../../../../utils/auth'
 import { Page as PageType } from '@prisma/client'
 import { Page } from '../../../../layout'
-import dynamic from 'next/dynamic'
 import { Box } from '@mui/material'
-import axios from 'axios'
+import prisma from '../../../../utils/prisma'
+import dynamic from 'next/dynamic'
 
-const GrapesjsReact = dynamic(
-  () =>
-    import('grapesjs-preset-webpage')
-      .then(() => import('grapesjs-react'))
-      .then((mod) => mod.GrapesjsReact) as any,
-  {
-    ssr: false
-  }
-) as React.FC<any>
+const PageEditor = dynamic(() => import('../../../../components/PageEditor'), {
+  ssr: false
+})
 
 type Props = {
   page: PageType
@@ -28,69 +22,29 @@ const Editor: Page<Props> = ({ page }) => {
         height: '100vh'
       }}
     >
-      <GrapesjsReact
-        id={`editor-${page.id}`}
-        height="100%"
-        width="100%"
-        onChange={() => {
-          console.log('afd')
-        }}
-        onDestroy={() => {
-          console.log('destroyed')
-        }}
-        onInit={(editor: any) => {
-          editor.StorageManager.add('vega', {
-            async load(keys: string[], clb: any, clbErr: any) {
-              try {
-                const { data } = await axios.get<Record<string, any>>(
-                  `/api/admin/pages/${page.id}`
-                )
-                const res: Record<string, any> = {}
-                keys.forEach((key) => {
-                  const value = data[key]
-                  if (value) {
-                    res[key] = value
-                  }
-                })
-
-                clb(res)
-              } catch (e) {
-                clbErr(e)
-              }
-            },
-            async store(data: any, clb: any, clbErr: any) {
-              try {
-                await axios.post(`/api/admin/pages/${page.id}/store`, {
-                  components: JSON.parse(data['gjs-components']),
-                  styles: JSON.parse(data['gjs-styles'])
-                })
-                clb()
-              } catch (e) {
-                clbErr(e)
-              }
-            }
-          })
-          console.log(editor)
-        }}
-        plugins={['gjs-preset-website', 'gjs-blocks-basic']}
-        storageManager={{
-          type: 'vega',
-          autoload: true
-        }}
-      />
+      <PageEditor data={page.data} id={page.id} />
     </Box>
   )
 }
 
-export const getServerSideProps = requireAdmin<Props>(async () => {
+export const getServerSideProps = requireAdmin<Props>(async (ctx) => {
+  const id = ctx.query.id as string
+
+  const page = await prisma.page.findUnique({
+    where: {
+      id
+    }
+  })
+
+  if (!page) {
+    return {
+      notFound: true
+    }
+  }
+
   return {
     props: {
-      page: {
-        data: [],
-        id: '12341234',
-        slug: '/test',
-        title: 'Test'
-      }
+      page
     }
   }
 })
